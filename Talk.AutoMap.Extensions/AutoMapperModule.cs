1823a63a16f635cbaa73874b6f06fb381d5997db
benny <zhaopeiym@163.com>
2017-02-27 13:19:27 +0800

    Talk.AutoMap.Extensions

---
+++ Talk.AutoMap.Extensions/AutoMapperModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talk.AutoMap.Extensions
+{
+    public static class AutoMapperModule
+    {
+        private static Type[] AttributeTypes
+        {
+            get
+            {
+                return new Type[] { typeof(AutoMapAttribute), typeof(AutoMapFromAttribute), typeof(AutoMapToAttribute) };
+            }
+        }
+        public static void Init(IEnumerable<Type> _type)
+        {
+            var types = _type.Where(type => AttributeTypes.Any(t => type.IsDefined(t)));
+            AutoMapperHelper.CreateMap(types, AttributeTypes);
+        }
+    }
+}
