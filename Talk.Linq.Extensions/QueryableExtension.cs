f68875e5da95d285eb53dc482bf513677035e29a
benny <zhaopeiym@163.com>
2017-03-03 18:09:23 +0800

    单元测试

--- Talk.Linq.Extensions/QueryableExtension.cs
+++ Talk.Linq.Extensions/QueryableExtension.cs
@@ -70,6 +70,27 @@ namespace Talk.Linq.Extensions
         }
 
         /// <summary>
+        /// 去重
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IQueryable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            //return collection.GroupBy(groupFunc).Select(group => group.First());
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        /// <summary>
         /// 拼接and 条件语句
         /// </summary>
         /// <typeparam name="T"></typeparam>
