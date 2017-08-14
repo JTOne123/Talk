fcc1fe25c9ba4ab5ec1c3f5a8fc2ec2bc6cf5eaf
benny <zhaopeiym@163.com>
2017-08-14 22:03:44 +0800

    轮子  缓存。

---
+++ Talk.Cache.Tests/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xunit;
+
+namespace Talk.Cache.Tests
+{
+
+    public class Class1
+    {
+
+        [Fact]
+        public void test()
+        {
+            //  Assert.Equal(obj2.Name, "张三"); 
+            EasyCache<string> a = new EasyCache<string>();
+            a.AddData("k", new CacheData<string>()
+            {
+                ExpirationTime = DateTime.Now.AddDays(1),
+                Data = "aaa",
+            });
+
+            a.AddData("j", new CacheData<string>()
+            {
+                ExpirationTime = DateTime.Now.AddDays(1),
+                Data = "ccc",
+            });
+            var vvv = a.GetData("k");
+            Assert.Equal(a.GetData("k")?.Data, "aaa");
+            Assert.Equal(a.GetData("j")?.Data, "ccc");
+
+            EasyCache<string> z = new EasyCache<string>();
+            Assert.Equal(z.GetData("k")?.Data, "aaa");
+
+            EasyCache<int> b = new EasyCache<int>();
+            b.AddData("b", new CacheData<int>()
+            {
+                ExpirationTime = DateTime.Now.AddDays(1),
+                Data = 111,
+            });
+
+            Assert.Equal(b.GetData("b")?.Data, 111);
+
+            EasyCache<int> index = new EasyCache<int>();
+            index.AddData("v", new CacheData<int>()
+            {
+                ExpirationTime = DateTime.Now.AddDays(1),
+                Data = 1,
+            });
+
+            ExecuteIndex e = new ExecuteIndex("ind", DateTime.Now.AddDays(2));
+            Assert.Equal(e.GetIndex(), 1);
+            Assert.Equal(e.GetIndex(), 2);
+            Assert.Equal(e.GetIndex(), 3);
+        }
+
+    }
+}
---
+++ Talk.Cache/EasyCache.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Talk.Cache
+{
+    public class CacheData<T>
+    {
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        public DateTime ExpirationTime { get; set; }
+
+        /// <summary>
+        /// 要存储的数据
+        /// </summary>
+        public T Data { get; set; }
+    }
+
+    public class EasyCache<T>
+    {
+        public static Dictionary<string, CacheData<T>> list = new Dictionary<string, CacheData<T>>();
+
+        public static object lockObj = new object();
+
+        /// <summary>
+        /// 添加数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="cacheData"></param>
+        public void AddData(string key, CacheData<T> cacheData)
+        {
+            lock (lockObj)
+            {
+                list.Add(key, cacheData);
+            }
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="cacheData"></param>
+        /// <returns></returns>
+        public bool ModifyData(string key, CacheData<T> cacheData)
+        {
+            var keys = list.Where(t => t.Value.ExpirationTime <= DateTime.Now).Select(t => t.Key).ToArray();
+            lock (lockObj)
+            {
+                foreach (var k in keys)
+                {
+                    list.Remove(k);
+                }
+                if (!list.ContainsKey(key))
+                    return false;
+                list[key] = cacheData;
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 如果没有取到 则返回null
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public CacheData<T> GetData(string key)
+        {
+            var keys = list.Where(t => t.Value.ExpirationTime <= DateTime.Now).Select(t => t.Key).ToArray();
+            lock (lockObj)
+            {
+                foreach (var k in keys)
+                {
+                    list.Remove(k);
+                }
+                if (list.ContainsKey(key))
+                    return list[key];
+                return null;
+            }
+        }
+
+    }
+
+    public class ExecuteIndex
+    {
+        private EasyCache<long> easy;
+        public string _key;
+        public ExecuteIndex(string key, DateTime time)
+        {
+            _key = key;
+            easy = new EasyCache<long>();
+            easy.AddData(key, new CacheData<long>()
+            {
+                Data = 0,
+                ExpirationTime = time,
+            });
+        }
+
+        public long GetIndex()
+        {
+            easy.ModifyData(_key, new CacheData<long>()
+            {
+                Data = easy.GetData(_key).Data + 1,
+                ExpirationTime = easy.GetData(_key).ExpirationTime,
+            });
+            return easy.GetData(_key).Data;
+        }
+    }
+}
