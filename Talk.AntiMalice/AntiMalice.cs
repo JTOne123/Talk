bae73dc8276869cd9b32570d175fc357d9009bb0
benny <zhaopeiym@163.com>
2017-08-22 22:06:05 +0800

    反捣乱

--- Talk.AntiMalice/AntiMalice.cs
+++ Talk.AntiMalice/AntiMalice.cs
@@ -11,7 +11,7 @@ namespace Talk.AntiMalice
     /// </summary>
     public static class AntiMalice
     {
-        //private readonly static string defaultValue = "defaltValue";
+        private readonly static string defaultValue = "defaltValue";
         private readonly static string key = "talkantimaliceXX";
 
         /// <summary>
@@ -28,6 +28,14 @@ namespace Talk.AntiMalice
                 httpContext.Response.Cookies.Append(key, value);
             }
         }
+
+        public class AntiMaliceToken
+        {
+            public long Num { get; set; }
+            public int Type { get; set; }
+            public bool IsOk { get; set; }
+        }
+
         /// <summary>
         /// 验证Token
         /// </summary>
@@ -36,44 +44,41 @@ namespace Talk.AntiMalice
         /// <param name="nsecond">未标记请求请求可以访问次数</param>
         /// <param name="usecond">标记请求可以访问次数（两分钟后清除标记）</param>
         /// <returns></returns>
-        public static bool ValidateToken(this HttpContext httpContext, int interval = 30, int nsecond = 15, int usecond = 5)
+        public static AntiMaliceToken ValidateToken(this HttpContext httpContext, int interval = 30, int nsecond = 15, int usecond = 5)
         {
             var antimaliceValue = httpContext.Request.Cookies.FirstOrDefault(t => t.Key == key).Value;
-            if (string.IsNullOrWhiteSpace(antimaliceValue))//如果没有cookie           
-                return false;
+            antimaliceValue = string.IsNullOrWhiteSpace(antimaliceValue) ? defaultValue : antimaliceValue;
             var time = DateTime.Now.AddSeconds(interval) - DateTime.Now;
-            ExecuteNum num = new ExecuteNum(antimaliceValue, time);
-            if (string.IsNullOrWhiteSpace(antimaliceValue))//证明是没有标记的请求
+            ExecuteNum executeNum = new ExecuteNum(antimaliceValue, time);
+            var num = executeNum.GetNum();
+            var antiMaliceToken = new AntiMaliceToken()
             {
-                if (num.GetNum() >= nsecond)//一定时间内不能超过多少次
-                    return false;
+                Num = num,
+                IsOk = true
+            };
+            if (antimaliceValue == defaultValue)//证明是没有标记的请求
+            {
+                antiMaliceToken.Type = 0;
+                httpContext.SetToken();
+                if (num >= nsecond)//一定时间内不能超过多少次                
+                    antiMaliceToken.IsOk = false;
             }
             else
             {
-                if (num.GetNum() >= usecond)//有标记也不能频繁操作
-                    return false;
-                var valueString = antimaliceValue.DES3Decrypt(key);
-                var values = valueString.Split('|');
-                if (values.Length != 2 || values[0] != "Talke")
-                    return false;
-                else if (DateTime.Parse(values[1]).AddMinutes(2) <= DateTime.Now)//清除2分钟前的标记                
-                    httpContext.Response.Cookies.Delete(key);
+                antiMaliceToken.Type = 1;
+                if (executeNum.GetNum() > usecond)//有标记也不能频繁操作                
+                    antiMaliceToken.IsOk = false;
+                else
+                {
+                    var valueString = antimaliceValue.DES3Decrypt(key);
+                    var values = valueString.Split('|');
+                    if (values.Length != 2 || values[0] != "Talke")
+                        antiMaliceToken.IsOk = false;
+                    else if (DateTime.Parse(values[1]).AddMinutes(2) <= DateTime.Now)//清除2分钟前的标记                
+                        httpContext.Response.Cookies.Delete(key);
+                }
             }
-            return true;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="httpContext"></param>
-        /// <param name="chanceNum">随机通过概率</param>
-        /// <returns></returns>
-        public static bool GetChance(this HttpContext httpContext, int chanceNum = 2)
-        {
-            var isAntiMalice = httpContext.ValidateToken();
-            if (!isAntiMalice)
-                return new Random().Next(100000) % chanceNum == 0;
-            return true;
-        }
+            return antiMaliceToken;
+        } 
     }
 }
