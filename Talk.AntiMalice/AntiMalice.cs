bf766bfa57c57a202a224bd25689515173788f05
benny <zhaopeiym@163.com>
2017-08-22 14:47:11 +0800

    dev

--- Talk.AntiMalice/AntiMalice.cs
+++ Talk.AntiMalice/AntiMalice.cs
@@ -11,29 +11,39 @@ namespace Talk.AntiMalice
     /// </summary>
     public static class AntiMalice
     {
-        private readonly static string defaultValue = "defaltValue";
+        //private readonly static string defaultValue = "defaltValue";
+        private readonly static string key = "talkantimaliceXX";
+
         /// <summary>
-        /// 
+        /// 设置Token
         /// </summary>
         /// <param name="httpContext"></param>
-        /// <param name="interval">时间间隔</param>
-        /// <param name="nsecond">未标记请求请求可以访问次数</param>
-        /// <param name="usecond">标记请求可以访问次数（两分钟后清除标记）</param>
-        /// <returns></returns>
-        public static bool GetValue(this HttpContext httpContext, int interval = 30, int nsecond = 15, int usecond = 5)
+        public static void SetToken(this HttpContext httpContext)
         {
-            var key = "talkantimaliceXX";
             var antimaliceValue = httpContext.Request.Cookies.FirstOrDefault(t => t.Key == key).Value;
             if (string.IsNullOrWhiteSpace(antimaliceValue))//如果没有cookie
             {
                 var value = "Talke|" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
                 value = value.DES3Encrypt(key);
                 httpContext.Response.Cookies.Append(key, value);
-                antimaliceValue = defaultValue;
             }
+        }
+        /// <summary>
+        /// 验证Token
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <param name="interval">时间间隔</param>
+        /// <param name="nsecond">未标记请求请求可以访问次数</param>
+        /// <param name="usecond">标记请求可以访问次数（两分钟后清除标记）</param>
+        /// <returns></returns>
+        public static bool ValidateToken(this HttpContext httpContext, int interval = 30, int nsecond = 15, int usecond = 5)
+        {
+            var antimaliceValue = httpContext.Request.Cookies.FirstOrDefault(t => t.Key == key).Value;
+            if (string.IsNullOrWhiteSpace(antimaliceValue))//如果没有cookie           
+                return false;
             var time = DateTime.Now.AddSeconds(interval) - DateTime.Now;
             ExecuteNum num = new ExecuteNum(antimaliceValue, time);
-            if (antimaliceValue == defaultValue)//证明是没有标记的请求
+            if (string.IsNullOrWhiteSpace(antimaliceValue))//证明是没有标记的请求
             {
                 if (num.GetNum() >= nsecond)//一定时间内不能超过多少次
                     return false;
@@ -60,9 +70,9 @@ namespace Talk.AntiMalice
         /// <returns></returns>
         public static bool GetChance(this HttpContext httpContext, int chanceNum = 2)
         {
-            var isAntiMalice = httpContext.GetValue();
-            if (!isAntiMalice)            
-                return new Random().Next(100000) % chanceNum == 0;            
+            var isAntiMalice = httpContext.ValidateToken();
+            if (!isAntiMalice)
+                return new Random().Next(100000) % chanceNum == 0;
             return true;
         }
     }
